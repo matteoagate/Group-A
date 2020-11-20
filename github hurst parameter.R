9191c3aaf1948d911a64d1e4dc1a968bacddf341
andreagulizia <74717432+andreagulizia@users.noreply.github.com>
2020-11-20 19:01:14 +0100

    Add files via upload
    Lo script è da migliorare. In questa bozza si parte da un'ipotesi di divisione della serie storica iniziale in 4, ma sarebbe meglio poterla dividere automaticamente in n-parti.
    Il detrending della serie non mi convince, in quanto potrebbero essere coinvolti più parametri, e non solo uno per ogni sub-serie. Deve essere controllata la correttezza della stima del parametro di Hurst. Non c'è possibilità di inserire una data

---
+++ github hurst parameter.R
@@ -0,0 +1,105 @@
+# choose a series
+
+apple <- read.csv("AAPL-10.csv")
+apple <- na.omit(apple)
+appleadj <- as.numeric(apple$Adj.Close)
+returns <- diff(log(appleadj))*100
+
+# Divide the series of N length in d subseries of length n
+# We establish the number of d, in our case it is 4, so we devide returns in 4. The length of n is
+# 2517
+
+returns <- returns[-10069]
+date <- apple$Date
+date <- date[-1]
+date <- date[-10069]
+dataframe <- data.frame(date, returns)
+library(dplyr)
+returns1 <- dataframe %>% slice(1:2517)
+returns2 <- dataframe %>% slice(2518:5034)
+returns3 <- dataframe %>% slice(5035:7551)
+returns4 <- dataframe %>% slice(7552:10068)
+
+
+# Creation of new cumulative time series
+Y1 <- c()
+for (i in 1:length(returns1$returns)) {
+  Y1[i] <- sum(returns1$returns[1:i])
+}
+Y1
+
+Y2 <- c()
+for (i in 1:length(returns2$returns)) {
+  Y2[i] <- sum(returns2$returns[1:i])
+}
+Y2
+
+Y3 <- c()
+for (i in 1:length(returns3$returns)) {
+  Y3[i] <- sum(returns3$returns[1:i])
+}
+Y3
+
+Y4 <- c()
+for (i in 1:length(returns4$returns)) {
+  Y4[i] <- sum(returns4$returns[1:i])
+}
+Y4
+
+# We fit a least square line for every series
+# we need a verctor that specifies the n
+
+t <- c(1:2517)
+Y1hat <- lm(Y1 ~ t)
+summary(Y1hat)
+b1 <- summary(Y1hat)$coefficients[1,1]
+a1 <- summary(Y1hat)$coefficients[2,1]
+
+Y2hat <- lm(Y2 ~ t)
+summary(Y2hat)
+b2 <- summary(Y2hat)$coefficients[1,1]
+a2 <- summary(Y2hat)$coefficients[2,1]
+
+Y3hat <- lm(Y3 ~ t)
+summary(Y3hat)
+b3 <- summary(Y3hat)$coefficients[1,1]
+a3 <- summary(Y3hat)$coefficients[2,1]
+
+Y4hat <- lm(Y4 ~ t)
+summary(Y4hat)
+b4 <- summary(Y4hat)$coefficients[1,1]
+a4 <- summary(Y4hat)$coefficients[2,1]
+
+# we compute the root root mean square fluctuation of the detrended series
+
+Y1m <- (Y1 - a1 - b1)
+Y1m2 <- (Y1 - a1 - b1)^(2)
+sumY1m2 <- sum(Y1m2)
+F1 <- (sumY1m2/2517)^(1/2)
+
+Y2m <- (Y2 - a2 - b2)
+Y2m2 <- (Y2 - a2 - b2)^(2)
+sumY2m2 <- sum(Y2m2)
+F2 <- (sumY2m2/2517)^(1/2)
+
+Y3m <- (Y3 - a3 - b3)
+Y3m2 <- (Y3 - a3 - b3)^(2)
+sumY3m2 <- sum(Y3m2)
+F3 <- (sumY3m2/2517)^(1/2)
+
+Y4m <- (Y4 - a4 - b4)
+Y4m2 <- (Y4 - a4 - b4)^(2)
+sumY4m2 <- sum(Y4m2)
+F4 <- (sumY4m2/2517)^(1/2)
+
+# At this point we calculate the mean of the Fm and we find the Hurst parameter,
+# looking at the coeffincient of the last regresson done below
+
+
+Fn <- (F1 + F2 + F3 + F4)/4
+
+prova<-data_frame(Y1m, Y2m, Y3m, Y4m)
+head(final_prova)
+final_prova<- prova %>% mutate(mean= ((Y1m+Y2m+Y3m+Y4m))^2/(4*2517))
+
+lm(log(final_prova$mean)~log(t))%>% summary()
